d4d00c07fdc1ac0a4399cf49672ffc7ef191f9bb
Rishueee <134083360+Rishueee@users.noreply.github.com>
2023-09-29 17:22:29 +0530

    Update game.js

--- game.js
+++ game.js
@@ -90,5 +90,6 @@ makeZombie(zombieId);
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min)) + min;
+  
 }
